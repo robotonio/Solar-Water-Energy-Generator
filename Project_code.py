b24692053154a6cc7e6c731d9692e5bc4cc314bc
Rick Sanchez <116969974+0x7269636B@users.noreply.github.com>
2023-01-22 12:27:30 +0200

    Update Project_code.py

--- Project_code.py
+++ Project_code.py
@@ -12,33 +12,76 @@ class Project:
         # Values
         self.pump_speed = 0
         self.temp = 0
-        self.bat_voltage = 0
+        self.bat_voltage = 3.92
         self.gen_voltage = 0
-        self.charging_status = 'Not Charging'
+        self.charging_status = 'Charging' #'Not Charging'
         self.charge = 100
+        self.fan = 0#1 if fan runs
+        self.pump = 0#1 if pump runs
         # i2c init
         I2C_LCD1602.LcdInit(39)
         I2C_LCD1602.clear()
         I2C_LCD1602.clear()
+        #init screen 
         I2C_LCD1602.show_string("Moving Minds", 2, 0)
-        I2C_LCD1602.show_string("Project!", 4, 1)
-        #set H bridge rotation  
-        pins.digital_write_pin(DigitalPin.P1, 0)
-        pins.digital_write_pin(DigitalPin.P2, 1)
-        pins.analog_write_pin(AnalogPin.P0, 0)
-    
+        I2C_LCD1602.show_string("Project!", 4, 1)        
+        #init pump and fan
+        self.turn_off_fan()
+        self.turn_off_pump()
+
+        self.set_battery_voltage()
+        self.set_charging_status()
+
     def get_temp(self):
-        self.temp = 45
-    
+        val = smarthome.read_temperature(TMP36Type.TMP36_TEMPERATURE_C, AnalogPin.P0)
+        self.temp = val
+
     def set_motor_speed(self, speed=0): 
         self.pump_speed = 10*speed
         pins.analog_write_pin(AnalogPin.P0, self.pump_speed)
-     
+
+    def set_battery_voltage(self):
+        pass
+            
+    def set_charging_status(self):
+        if self.gen_voltage > 0:
+            self.charging_status = "Charging"
+        else:
+            self.charging_status = "Not Charging"
+
     def get_battery_voltage(self):  
-        self.bat_voltage = 3.89
-     
+        self.bat_voltage = 12.5
+    
+    def get_battery_percentage(self):
+        val = self.bat_voltage
+        return (val/14.2)*100
+
+    def turn_off_fan(self):
+        self.fan = 1
+        pins.digital_write_pin(DigitalPin.P2, 1)
+
+    def turn_on_fan(self):
+        self.fan = 0
+        pins.digital_write_pin(DigitalPin.P2, 0)
+
+    def turn_off_pump(self):
+        self.pump = 1
+        self.pump_speed = 0
+        pins.digital_write_pin(DigitalPin.P12, 1)
+
+    def turn_on_pump(self):
+        self.pump = 0
+        self.pump_speed = 100
+        pins.digital_write_pin(DigitalPin.P12, 0)
+
     def get_generator_voltage(self):  
-        self.gen_voltage = 10.2
+        analog_input = pins.analog_read_pin(AnalogPin.P1)
+        voltage = Math.map(analog_input, 0, 1023, 0, 10)
+        self.gen_voltage = Math.round_with_precision(voltage, 2)
+                
+    def print_value(self, value):
+        I2C_LCD1602.clear()
+        I2C_LCD1602.show_number(value, 0, 0)
      
     def update_values(self, rep): 
         if rep == 1:
@@ -68,9 +111,9 @@ class Project:
             else:
                 I2C_LCD1602.ShowString(self.charging_status, 4, 1) 
 
-        
 SWEG = Project()
 counter=1
+threshold = 50
 
 def on_button_pressed_a():
     global counter
@@ -94,8 +137,13 @@ def on_button_pressed_b():
             
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-
 def on_forever():
-    SWEG.set_motor_speed(0)
+
+    #SWEG.get_generator_voltage()
+    SWEG.get_temp()
+    if SWEG.temp < threshold:
+        SWEG.turn_on_pump()
+    else:
+        SWEG.turn_off_pump()
 
 basic.forever(on_forever)
